02d5be7295b8b3fea4931607ab9891dd8d3fa189
NotNite <hi@notnite.com>
2022-07-01 23:16:56 -0400

    init

---
+++ src/client.rs
@@ -0,0 +1,141 @@
+use anyhow::anyhow;
+use chrono::{DateTime, Local, TimeZone, Utc};
+use reqwest::StatusCode;
+use serde::{Deserialize, Serialize};
+use std::env;
+
+fn get_api_key() -> String {
+    env::var("NNC_API_KEY").expect(
+        "couldn't NotNetCoin API key - make sure to set the `NNC_API_KEY` environment variable",
+    )
+}
+
+// what a mess
+#[derive(Deserialize, Debug)]
+struct UserInfo {
+    id: String,
+    username: String,
+    discriminator: String,
+
+    #[serde(rename = "apiKey")]
+    api_key: String,
+    balance: u32,
+}
+
+#[derive(Serialize)]
+struct TransactionRequest {
+    to: String,
+    amount: u32,
+}
+
+#[derive(Deserialize, Debug)]
+struct TransactionInfo {
+    id: String,
+    from: String,
+    to: String,
+    amount: u32,
+}
+
+#[derive(Deserialize, Debug)]
+struct RequestError {
+    error: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct DailyError {
+    error: String,
+    reset: i64,
+}
+
+#[derive(Deserialize, Debug)]
+
+struct DailyInfo {
+    amount: u8,
+}
+
+pub struct Client {
+    http_client: reqwest::blocking::Client,
+}
+
+impl Client {
+    pub fn new() -> Self {
+        Client {
+            http_client: reqwest::blocking::Client::new(),
+        }
+    }
+
+    pub fn check_balance(&mut self) -> anyhow::Result<()> {
+        let api_key = get_api_key();
+
+        let res = self
+            .http_client
+            .get("https://nnc.n2.pm/me")
+            .header("Authorization", api_key)
+            .send()?;
+
+        if res.status() != StatusCode::OK {
+            let err: RequestError = res.json()?;
+            return Err(anyhow!("{}", err.error));
+        }
+
+        let json: UserInfo = res.json()?;
+
+        println!("You have {} NNC.", json.balance);
+
+        Ok(())
+    }
+
+    pub fn send_money(&mut self, to: String, amount: u32) -> anyhow::Result<()> {
+        let api_key = get_api_key();
+
+        let req = TransactionRequest { to, amount };
+
+        let res = self
+            .http_client
+            .post("https://nnc.n2.pm/transactions")
+            .header("Authorization", api_key)
+            .json(&req)
+            .send()?;
+
+        if res.status() != StatusCode::OK {
+            let err: RequestError = res.json()?;
+            return Err(anyhow!("{}", err.error));
+        }
+
+        let json: TransactionInfo = res.json()?;
+
+        println!("Sent {} NNC to {}.", json.amount, json.to);
+
+        Ok(())
+    }
+
+    pub fn claim_daily(&mut self) -> anyhow::Result<()> {
+        let api_key = get_api_key();
+
+        let res = self
+            .http_client
+            .get("https://nnc.n2.pm/daily")
+            .header("Authorization", api_key)
+            .send()?;
+
+        if res.status() == StatusCode::BAD_REQUEST {
+            let err: DailyError = res.json()?;
+            let dt = Utc.timestamp(err.reset / 1000, 0);
+            let what: DateTime<Local> = DateTime::from(dt);
+            return Err(anyhow!(
+                "Not yet ready. Try again at {}.",
+                what.to_rfc2822()
+            ));
+        }
+
+        if res.status() == 401 {
+            let err: RequestError = res.json()?;
+            return Err(anyhow!("{}", err.error));
+        }
+
+        let json: DailyInfo = res.json()?;
+        println!("You got {} NNC.", json.amount);
+
+        Ok(())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use clap::{Parser, Subcommand};
+use std::error::Error;
+
+mod client;
+use client::Client;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Check your balance.
+    Balance,
+    /// Send money to a user.
+    Send {
+        /// The recipient's Discord ID.
+        to: String,
+        /// The amount of NotNetCoin to send.
+        amount: u32,
+    },
+    /// Claim your daily NotNetCoin.
+    Daily,
+    /// Log in to NotNetCoin.
+    Login,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    color_eyre::install()?;
+
+    let cli = Cli::parse();
+    let mut client = Client::new();
+
+    match cli.command {
+        Commands::Balance => client.check_balance()?,
+        Commands::Send { to, amount } => client.send_money(to, amount)?,
+        Commands::Daily => client.claim_daily()?,
+        Commands::Login => open::that("https://discord.com/api/oauth2/authorize?client_id=991697126784520293&redirect_uri=https%3A%2F%2Fnnc.n2.pm%2Foauth&response_type=code&scope=identify")?
+    }
+
+    Ok(())
+}
